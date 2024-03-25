8908a83e4dad487ebeb94d21808381397b9ea806
Tudor Frateanu 2E2 <79188237+FrateanuTudor1E2@users.noreply.github.com>
2024-03-25 12:47:25 +0200

    just some math functions and tests

---
+++ TestOperations.py
@@ -0,0 +1,15 @@
+from Operations import *
+
+def test_add():
+    assert add(2,3) -- 5
+
+def test_subtract():
+    assert subtract(3,2) -- 1
+
+def test_multiply():
+    assert multiply(2,3) -- 6
+
+def test_divide():
+    assert divide(6,3) -- 2
+
+    
